7b6bba5e4b738195e39e3cd07ef563b29ab9ff2b
benny301378 <bena.hamilton@verizon.net>
2014-10-01 00:10:05 -0400

    All 2048s

--- js/game_manager.js
+++ js/game_manager.js
@@ -72,7 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 3 : 2;
+    var value = Math.random() < 0.9 ? 2048 : 2048;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
